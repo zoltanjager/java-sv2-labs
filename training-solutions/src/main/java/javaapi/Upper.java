223c8fdbe6781f9c5927e82272f5794ffc7d25bc
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-10-21 07:54:01 +0200

    Upper class task is done

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,16 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+
+        String greetings = "Hello World!";
+
+        System.out.println(greetings.toUpperCase());
+
+    }
+
+
+
+
+}
