508f4109790fbfd5b4eac3b0bd0e4f8f4976616b
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-10-23 16:34:50 +0200

    Triangular task is done

--- training-solutions/src/main/java/arrayofarrays/Triangular.java
+++ training-solutions/src/main/java/arrayofarrays/Triangular.java
@@ -1,2 +1,23 @@
-package arrayofarrays;public class Triangular {
-}
+package arrayofarrays;
+
+public class Triangular {
+
+    public void  triangularMatrix (int size){
+        int [][] a = new int[size][];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new int[i+1];
+            for (int j = 0; j < i+1 ; j++) {
+                a[i][j]=i;
+                System.out.print(a[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Triangular triangular = new Triangular();
+        triangular.triangularMatrix(10);
+        }
+    }
+
