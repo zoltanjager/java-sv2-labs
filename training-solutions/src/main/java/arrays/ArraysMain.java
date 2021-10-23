a557d1dc410d130836b0d0445e4dc9be39df3d00
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-10-23 19:26:34 +0200

    sameTempValues task is done

--- training-solutions/src/main/java/arrays/ArraysMain.java
+++ training-solutions/src/main/java/arrays/ArraysMain.java
@@ -20,12 +20,20 @@ public class ArraysMain {
         return Arrays.deepToString(a);
     }
 
+    public boolean sameTempValues(double[] day, double[] anotherDay){
+        double[] ArraysDay = Arrays.copyOfRange(day, 0, day.length);
+        double[] ArraysAnotherDay = Arrays.copyOfRange(anotherDay, 0, anotherDay.length);
+        return Arrays.equals(ArraysDay,ArraysAnotherDay);
+    }
+
     public static void main(String[] args) {
 
         ArraysMain arraysMain = new ArraysMain();
         System.out.println(arraysMain.numberOfDaysAsString());
         System.out.println(arraysMain.multiplicationTableAsString(4));
 
+        System.out.println(arraysMain.sameTempValues(new double[]{12.2, 13.2, 14.2, 15.4, 18.5, 20},
+                                                     new double[]{12.2, 13.4, 14.2, 15.4, 18.5, 20}));
     }
 
 }
