72f3a271da72bf48d7e1802c22cfa34942e33307
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-10-24 19:53:42 +0200

    Math methods  task is done

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,22 @@
+package math.math;
+
+public class MathMethods {
+    public static void main(String[] args) {
+
+        System.out.println(Math.max(5, 6));
+        System.out.println(Math.min(5, 6));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.round(2.5678));
+        System.out.println(Math.abs(-5));
+        System.out.println(Math.negateExact(1));
+        System.out.println(Math.addExact(5, 6));
+        System.out.println(Math.subtractExact(5, 6));
+        System.out.println(Math.multiplyExact(5, 6));
+        System.out.println(Math.pow(5.0, 6.0));
+        System.out.println(Math.incrementExact(5));
+        System.out.println(Math.decrementExact(5));
+        System.out.println(Math.random());
+
+    }
+}
