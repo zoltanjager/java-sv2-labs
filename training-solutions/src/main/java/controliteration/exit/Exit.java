12d5b400eaa9d98298779c5309c9c55a9677c010
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-11-05 19:01:33 +0100

    Exit task is done

---
+++ training-solutions/src/main/java/controliteration/exit/Exit.java
@@ -0,0 +1,15 @@
+package controliteration.exit;
+
+import java.util.Scanner;
+
+public class Exit {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        do {
+            System.out.println("Az ügyfélszolgálat menüje jelenleg nem elérhető. Kilépés: x billentyű megnyomásával");
+            input = scanner.nextLine();
+        } while ( ! "x".equals(input) );
+    }
+}
