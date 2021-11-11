bae6bd7b3250ed4808f1425c61ce4bf7f22692b6
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-11-11 20:12:45 +0100

    Numbers task is done

---
+++ training-solutions/src/main/java/introexceptiontrace/numbers/NumbersMain.java
@@ -0,0 +1,12 @@
+package introexceptiontrace.numbers;
+
+import java.util.Arrays;
+
+public class NumbersMain {
+
+    public static void main(String[] args) {
+
+        Change change = new Change();
+        System.out.println(Arrays.toString(change.changeNumbers()));
+    }
+}
