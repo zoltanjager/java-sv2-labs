790b817a5e9b9b8e9665b1a5a14e95a9626c9d0a
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-11-11 19:49:48 +0100

    Person task is done

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Person.java
@@ -0,0 +1,36 @@
+package introexceptionmoreexceptions;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Person person = new Person(null, 32);
+
+        System.out.println(person);
+        System.out.println(person.getName().toUpperCase());
+
+    }
+}
