c0a1a4cd5dbe4e0d8d03c4a8f1bb6b0dbbe44244
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-11-27 14:56:18 +0100

    BMI task is done

---
+++ training-solutions/src/main/java/methodstructure/bmi/BodyMass.java
@@ -0,0 +1,38 @@
+package methodstructure.bmi;
+
+public class BodyMass {
+    private double weight;
+    private double height;
+
+    public BodyMass(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double getBodyMassIndex() {
+        return weight / ( height * height );
+    }
+
+    public BmiCategory getBody() {
+        if (getBodyMassIndex() < 18.5) {
+            return BmiCategory.UNDERWEIGHT;
+        } if (getBodyMassIndex() > 25) {
+            return BmiCategory.OVERWEIGHT;
+        } else {
+            return BmiCategory.NORMAL;
+        }
+    }
+
+    public boolean isThinnerThan (BodyMass other) {
+        return this.getBodyMassIndex() < other.getBodyMassIndex();
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+}
