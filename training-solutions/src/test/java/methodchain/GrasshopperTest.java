8aa55d0f5d5b4701f0c81fdc04f1757b04ee050c
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-11-28 19:40:42 +0100

    Grasshopper task is done

---
+++ training-solutions/src/test/java/methodchain/GrasshopperTest.java
@@ -0,0 +1,27 @@
+package methodchain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrasshopperTest {
+
+    Grasshopper grasshopper = new Grasshopper();
+
+    @Test
+
+    void testHopOnce() {
+        grasshopper.hopOnce(Direction.POSITIVE).hopOnce(Direction.NEGATIVE).hopOnce(Direction.POSITIVE);
+
+        assertEquals(1, grasshopper.getPosition());
+    }
+
+    @Test
+
+    void testMoveFromZeroToThreeWithFiveHops() {
+        grasshopper.moveFromZeroToThreeWithFiveHops();
+
+        assertEquals(3, grasshopper.getPosition());
+    }
+
+}
