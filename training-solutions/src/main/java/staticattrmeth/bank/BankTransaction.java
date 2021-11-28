c90f909f289e0cfd9e4992ffc8a201eeaee11a9e
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-11-28 19:43:00 +0100

    BankTransaction task is done

---
+++ training-solutions/src/main/java/staticattrmeth/bank/BankTransaction.java
@@ -0,0 +1,52 @@
+package staticattrmeth.bank;
+
+public class BankTransaction {
+
+    private static final long MIN_TRANSACTION_VALUE = 1;
+    private static final long MAX_TRANSACTION_VALUE = 10_000_000;
+    private static long countOfTransactions = 0;
+    private static long sumOfTransactions = 0;
+    private static long currentMinValue = MAX_TRANSACTION_VALUE;
+    private static long currentMaxValue = MIN_TRANSACTION_VALUE;
+
+    private long transactionValue;
+
+    public BankTransaction(long transactionValue) {
+        if (transactionValue <= MIN_TRANSACTION_VALUE && transactionValue >= MAX_TRANSACTION_VALUE) {
+            throw new IllegalArgumentException("A tranzakció nem feldolgozható! " + transactionValue);
+        }
+        countOfTransactions++;
+        sumOfTransactions += transactionValue;
+        if (transactionValue < currentMinValue) currentMinValue = transactionValue;
+        if (transactionValue > currentMaxValue) currentMaxValue = transactionValue;
+        this.transactionValue = transactionValue;
+    }
+
+    public static void initTheDay() {
+        countOfTransactions = 0;
+        sumOfTransactions = 0;
+        currentMinValue = MAX_TRANSACTION_VALUE;
+        currentMaxValue = MIN_TRANSACTION_VALUE;
+    }
+
+    public static long getAverageOfTransaction() {
+        return countOfTransactions == 0 ? 0 : Math.round((double) sumOfTransactions / countOfTransactions);
+    }
+
+    public static long getCurrentMinValue() {
+        return countOfTransactions == 0 ? 0 : currentMinValue;
+    }
+
+    public static long getCurrentMaxValue() {
+        return countOfTransactions == 0 ? 0 : currentMaxValue;
+    }
+
+    public static long getSumOfTransactions() {
+        return sumOfTransactions;
+    }
+
+    public long getTransactionValue() {
+        return transactionValue;
+    }
+
+}
