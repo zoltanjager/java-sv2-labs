f6cd70cd9a16104537b3f302194b137470b71c19
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-11-30 19:24:49 +0100

    ancestor task is done

---
+++ training-solutions/src/main/java/objectclass/ancestor/CommonAncestor.java
@@ -0,0 +1,36 @@
+package objectclass.ancestor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommonAncestor {
+    public static void main(String[] args) {
+
+        List<Object> objects = new ArrayList<>();
+        Fruit fruit = new Fruit();
+
+        objects.add(fruit);
+        Apple apple = new Apple();
+        objects.add(apple);
+        Starking starking = new Starking();
+        objects.add(starking);
+        Vegetable vegetable = new Vegetable();
+        objects.add(vegetable);
+        objects.add("alma");
+        objects.add(12);
+        objects.add("x");
+        LocalDate localDate = LocalDate.now();
+        objects.add(localDate);
+        int [] numbers = {1,2,3,4,5};
+        objects.add(numbers);
+        List<String> strings =  Arrays.asList("körte", "szilva", " banán" );
+        objects.add(strings);
+
+        System.out.println(objects);
+
+
+    }
+
+}
