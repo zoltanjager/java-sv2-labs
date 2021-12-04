b65f1ec590e86c476fe29ffbf35946d5e26faa82
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-12-04 18:40:15 +0100

    Box task is done

---
+++ training-solutions/src/main/java/interfacedependencyinversion/box/Box.java
@@ -0,0 +1,6 @@
+package interfacedependencyinversion.box;
+
+public interface Box {
+    Size getSize();
+    int getPrice();
+}
---
+++ training-solutions/src/main/java/interfacedependencyinversion/box/CourierCompany.java
@@ -0,0 +1,23 @@
+package interfacedependencyinversion.box;
+
+public class CourierCompany {
+
+    public static void main(String[] args) {
+         Box smallBox = new SmallBox();
+         Box mediumBox = new MediumBox();
+         Box largeBox = new LargeBox();
+         Box extraLargeBox = new ExtraLargeBox();
+
+        System.out.println(smallBox.getPrice());
+        System.out.println(smallBox.getSize());
+
+        System.out.println(mediumBox.getPrice());
+        System.out.println(mediumBox.getSize());
+
+        System.out.println(largeBox.getPrice());
+        System.out.println(largeBox.getSize());
+
+        System.out.println(extraLargeBox.getPrice());
+        System.out.println(extraLargeBox.getSize());
+    }
+}
