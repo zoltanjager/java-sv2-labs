64bfe7c10103d1e4f27ee8d59f74fbd46bd3bed6
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-12-05 10:30:27 +0100

    Typewriter task is done

---
+++ training-solutions/src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,17 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+    UPPERCASE {
+        @Override
+        TypeWriterState next() {
+            return TypeWriterState.LOWERCASE;
+        }
+    }, LOWERCASE {
+        @Override
+        TypeWriterState next() {
+            return TypeWriterState.UPPERCASE;
+        }
+    };
+
+    abstract TypeWriterState next();
+}
