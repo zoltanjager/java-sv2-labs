5a6e509d06e8d9e0ff917f0c2cc371c021655be3
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-12-11 19:08:54 +0100

    Vehicle task is done

---
+++ training-solutions/src/main/java/virtualmethod/vehicle/Van.java
@@ -0,0 +1,25 @@
+package virtualmethod.vehicle;
+
+public class Van extends Car{
+    private int cargoWeight;
+
+    public Van(int vehicleWeight, int numberOfPassenger, int cargoWeight) {
+        super(vehicleWeight, numberOfPassenger);
+        this.cargoWeight = cargoWeight;
+    }
+
+
+    @Override
+    public int getGrossLoad() {
+       return super.getGrossLoad() + cargoWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Van{" +
+                "cargoWeight=" + cargoWeight +
+                ", numberOfPassenger=" + numberOfPassenger +
+                ", vehicleWeight=" + getVehicleWeight() +
+                '}';
+    }
+}
