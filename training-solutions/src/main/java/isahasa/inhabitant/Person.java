50857273589e058116560595a3d03124085000fe
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2021-12-11 20:39:28 +0100

    Inhabitant task is done

---
+++ training-solutions/src/main/java/isahasa/inhabitant/Person.java
@@ -0,0 +1,15 @@
+package isahasa.inhabitant;
+
+public abstract class Person {
+
+    protected String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
