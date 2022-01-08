39f1958a7df4080e8186059e32f14944fec0f41a
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-01-08 10:06:24 +0100

    Genericusage is done

---
+++ training-solutions/src/main/java/genericsusage/withgenerics/Book.java
@@ -0,0 +1,13 @@
+package genericsusage.withgenerics;
+
+public class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
