020175fada734030a0a50d74bff68ff1954f3481
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-01-10 20:04:04 +0100

    Paper is done

---
+++ training-solutions/src/main/java/collectionsautoboxing/Paper.java
@@ -0,0 +1,43 @@
+package collectionsautoboxing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Paper {
+    private Map<String, Integer> paper = new HashMap<>();
+
+    public Map<String, Integer> getPaper() {
+        return new HashMap<>(paper);
+    }
+
+    public void putFurtherPaper(String className, int kilogramms){
+        if(!paper.containsKey(className)){
+            paper.put(className,0);
+        }
+        paper.put(className, paper.get(className)+kilogramms);
+    }
+
+    public String getWinnerClass(){
+        int max = Integer.MIN_VALUE;
+        for (Integer actual: paper.values()) {
+            if ( actual > max){
+                max=actual;
+            }
+        }
+        String result="";
+        for (Map.Entry entry : paper.entrySet()){
+            if(max==(Integer)entry.getValue()){
+                result= entry.getKey().toString();
+            }
+        }
+        return result;
+    }
+
+    public int getTotalWeight(){
+        int sum = 0;
+        for (int actual: paper.values()) {
+            sum += actual;
+        }
+        return sum;
+    }
+}
