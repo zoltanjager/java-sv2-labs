0f09d29c4ad8422e11f887a9605b2011486644b8
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-01-14 19:57:55 +0100

    CarDealer is done

---
+++ training-solutions/src/main/java/searching/CarDealer.java
@@ -0,0 +1,27 @@
+package searching;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarDealer {
+    private List<Car> cars = new ArrayList<>();
+
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+
+    public List<Car> getCarsOrderedByYearOfConstruction() {
+        List<Car> ordered = new ArrayList<>(cars);
+        ordered.sort(new CarConstructionComparator());
+        return ordered;
+    }
+
+
+    public List<Car> getCarsOrderedByPrice() {
+        List<Car> ordered = new ArrayList<>(cars);
+        ordered.sort(new CarPriceComparator());
+        return ordered;
+    }
+}
