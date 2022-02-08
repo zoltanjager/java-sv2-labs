d08ea2d2d33748572f74ad35b5663831e1e151af
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-02-08 21:01:32 +0100

    Activity project is done

--- training-solutions/src/main/java/activity/Track.java
+++ training-solutions/src/main/java/activity/Track.java
@@ -5,9 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 
