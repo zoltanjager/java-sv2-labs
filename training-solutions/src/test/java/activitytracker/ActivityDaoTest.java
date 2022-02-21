d29dd9da8b19caad6f4642a96aff4f0c98bb8bcb
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-02-21 22:47:15 +0100

    Trackpoint is done

--- training-solutions/src/test/java/activitytracker/ActivityDaoTest.java
+++ training-solutions/src/test/java/activitytracker/ActivityDaoTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mariadb.jdbc.MariaDbDataSource;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -65,4 +67,27 @@ class ActivityDaoTest {
 
         assertEquals(5, savedActivity.getId());
     }
+
+
+    @Test
+    void testSaveActivitywithTrackPointsEverythingIsOK() {
+        TrackPoint trackPoint1 = new TrackPoint(LocalDate.of(2021, 2, 24), 47.2181020, 18.5411940);
+        TrackPoint trackPoint2 = new TrackPoint(LocalDate.of(2021, 2, 24), 47.2181230, 18.5411780);
+        TrackPoint trackPoint3 = new TrackPoint(LocalDate.of(2020, 12, 14), 47.2302470, 18.5472280);
+        TrackPoint trackPoint4 = new TrackPoint(LocalDate.of(2020, 12, 14), 47.2302550, 18.5472310);
+        TrackPoint trackPoint5 = new TrackPoint(LocalDate.of(2020, 12, 14), 47.2302552, 18.5472312);
+        List<TrackPoint> trackpoints = Arrays.asList(trackPoint1, trackPoint2, trackPoint3, trackPoint4, trackPoint5);
+        Activity activity = new Activity(5, LocalDateTime.of(2022, 02, 20, 8, 14), "Kékes", Type.BIKING, trackpoints);
+
+        activityDao.saveActivityAndTrackPoints(activity);
+        Activity expected = activityDao.findActivityWithTrackPointsById(5);
+
+        assertTrue(activity.getStartTime().equals(expected.getStartTime()));
+        assertTrue(activity.getDesc().equals(expected.getDesc()));
+        assertTrue(activity.getType().equals(expected.getType()));
+        assertTrue(activity.getTrackPoints().size() == expected.getTrackPoints().size());
+        assertTrue(expected.getTrackPoints().equals(trackpoints));
+    }
+
+
 }
