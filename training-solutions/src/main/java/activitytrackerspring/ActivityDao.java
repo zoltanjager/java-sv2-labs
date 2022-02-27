7d5628ff1489303a7e0758e3e2d6be7428c2f69d
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-02-27 18:32:49 +0100

    ActivityDao spring-jdbc

--- training-solutions/src/main/java/activitytrackerspring/ActivityDao.java
+++ training-solutions/src/main/java/activitytrackerspring/ActivityDao.java
@@ -6,7 +6,9 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ActivityDao {
 
@@ -23,12 +25,30 @@ public class ActivityDao {
 
     public Activity findActivityById(long id) {
 
-        return jdbcTemplate.queryForObject("select * from activities where id=?;", (rs, rowNum) -> {
-            return new Activity(rs.getInt("id"),
+        return jdbcTemplate.queryForObject("select * from activities where id=?;", (rs, rowNum) ->
+            new Activity(rs.getInt("id"),
                     rs.getTimestamp("start_time").toLocalDateTime(),
                     rs.getString("activity_desc"),
-                    Type.valueOf(rs.getString("activity_type")));
-        }, id);
+                    Type.valueOf(rs.getString("activity_type")))
+        , id);
 
     }
+
+    public Activity findActivityById2(long id) {
+        List<Activity> result = jdbcTemplate.query("select * from activities where id=?;",
+                this::mapRowToActivity,
+                id);
+        return result.size() == 0 ?
+                null : result.get(0);
+    }
+
+
+    private Activity mapRowToActivity(ResultSet row, int rowNum) throws SQLException {
+        return new Activity(row.getInt("id"),
+                row.getTimestamp("start_time").toLocalDateTime(),
+                row.getString("activity_desc"),
+                Type.valueOf(row.getString("activity_type")));
+    }
+
+
 }
